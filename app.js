8c996e67bd13f706f2865218f0e6cddd0d11e3f2
Andy Wolff <andy.wolff@gmail.com>
2016-11-13 16:53:53 -0600

    fixed

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var print = require('./print');
 
 http.createServer(function (req, res) {
 		res.writeHead(200);
-		res.write(print.balance());
-		res.write(print.money());
+    res.write(print.balance() + print.money());
 		res.end();
 	})
 	.listen(3000);
 
  
-console.log('boo'); 
+console.log('listening on Port 3000'); 
